0fd217e72942c8e53af12557b41564912899bf14
Lasse <lasse.tammela@aalto.fi>
2020-02-10 17:27:40 +0200

    production preparations

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 require('./startup/cors')(app);
 require('./startup/routes')(app);
 require('./startup/db')();
+require('./startup/prod')(app);
 
 const port = process.env.PORT || config.get('port');
 app.listen(port, () => console.log(`Listening on port ${port}`));
